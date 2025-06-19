364a46851bc1f64baab1ff1e52725c7ffaa4e69f
PReshma-tp <reshma@testpress.in>
2025-06-19 12:25:34 +0530

    added 2 tables polls_choice, polls_question

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length= 200)
+    published_date = models.DateTimeField("published_date")
+
+class Choice (models.Model):
+    choice_text = models.CharField(max_length= 200)
+    vote = models.IntegerField(default=0)
+    question =  models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    

4e51414717baae8e3e8efdfbb5af643938256cad
PReshma-tp <reshma@testpress.in>
2025-06-20 11:50:08 +0530

    Fix typo in path

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index", views.index, name="index"),
-    path("<int: question_id", views.details, name="details"),
-    path("<int: question_id/result", views.result, name="result"),
-    path("<int: question_id/vote", views.vote, name="vote")
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.details, name="details"),
+    path("<int:question_id>/result/", views.result, name="result"),
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]

7e7182d44af9ab53b844ff6b30f808914fa7f3c5
PReshma-tp <reshma@testpress.in>
2025-06-20 11:18:22 +0530

    add vote view

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,10 @@ def index(request):
     return HttpResponse("Hello User:)) You are at Polls Index...")
 
 def details(request, question_id):
-    return HttpResponse("You are lokking at the question %s " % question_id)
+    return HttpResponse("You are looking at the question %s " % question_id)
 
 def result(request, question_id):
     return HttpResponse("You are looking at the results of question %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s" % question_id)

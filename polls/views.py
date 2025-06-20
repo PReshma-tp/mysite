50b53776f707e3bc780bafc4c1f31adc95e76f92
PReshma-tp <reshma@testpress.in>
2025-06-20 11:16:26 +0530

    add result view

--- polls/views.py
+++ polls/views.py
@@ -7,8 +7,4 @@ def details(request, question_id):
     return HttpResponse("You are lokking at the question %s " % question_id)
 
 def result(request, question_id):
-    return HttpResponse("You are looking at the results of the question %s " % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You are voting on the question %s" % question_id)
-
+    return HttpResponse("You are looking at the results of question %s" % question_id)

ef2a6a5c1965f6d261fe35887fc14f3132959c72
PReshma-tp <reshma@testpress.in>
2025-06-20 11:21:28 +0530

    add urlConfig to views (details, result, vote)

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("index", views.index, name="index"),
+    path("details", views.details, name="details"),
+    path("result", views.result, name="result"),
+    path("vote", views.vote, name="vote")
 ]

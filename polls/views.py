26c3672ac0fbae93bd032fd83f3a5403ee3233c6
PReshma-tp <reshma@testpress.in>
2025-06-20 11:13:38 +0530

    add details view

--- polls/views.py
+++ polls/views.py
@@ -2,3 +2,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello User:)) You are at Polls Index...")
+
+def details(request, question_id):
+    return HttpResponse("You are lokking at the question %s " % question_id)
+
+def result(request, question_id):
+    return HttpResponse("You are looking at the results of the question %s " % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on the question %s" % question_id)
+

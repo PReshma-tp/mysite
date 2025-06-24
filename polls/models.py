b7717c1dc4bf0d3881f2ac0c95d670300c1bef1f
PReshma-tp <reshma@testpress.in>
2025-06-24 12:58:43 +0530

    add custom method to Question model

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
 from django.db import models
+from django.utils import timezone 
+
 
 # Create your models here.
 class Question(models.Model):
@@ -7,6 +10,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        return self.published_date >= timezone.now()-datetime.timedelta(days=1)
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length= 200)

7f4cd486af920a72c8c8c39165061127bfadc4df
PReshma-tp <reshma@testpress.in>
2025-06-24 11:29:54 +0530

    modify result view to display result template

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,8 @@ def details(request, question_id):
     return render(request, 'polls/details.html', {"question": question})
 
 def result(request, question_id):
-    return HttpResponse("You are looking at the results of question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
